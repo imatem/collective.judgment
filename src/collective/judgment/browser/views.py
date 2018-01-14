f3d5aa8069d5e76b4fc7f00cde6f5f6e3d13637d
adriana-rv <adriana.rv@gmail.com>
2018-01-13 19:33:12 -0600

    Add promotion view

--- src/collective/judgment/browser/views.py
+++ src/collective/judgment/browser/views.py
@@ -5,3 +5,7 @@ from plone.dexterity.browser.view import DefaultView
 class PromotionView(DefaultView):
     """ The default view for talks
     """
+
+    def foo(self):
+        import pdb; pdb.set_trace()
+        return ''
