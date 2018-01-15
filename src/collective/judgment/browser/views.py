40161d393725bfd60678c1c1bf8acaae46f42309
adriana-rv <adriana.rv@gmail.com>
2018-01-15 16:38:49 -0600

    Add folder cdim view

--- src/collective/judgment/browser/views.py
+++ src/collective/judgment/browser/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from plone.dexterity.browser.view import DefaultView
+from Products.Five import BrowserView
+from plone import api
 
 class PromotionView(DefaultView):
     """ The default view for talks
@@ -9,3 +11,30 @@ class PromotionView(DefaultView):
     def foo(self):
         import pdb; pdb.set_trace()
         return ''
+
+
+class FolderCdimView(BrowserView):
+
+    def itembrains(self):
+        catalog = api.portal.get_tool(name='portal_catalog')
+        brains = catalog(
+            path=dict(query='/'.join(self.context.getPhysicalPath()), depth=1),
+            # sort_on='start'
+        )
+
+        return brains
+
+    def evaluations(self, brain):
+
+        values = {
+            'revisor1': 'approve',
+            'revisor2': 'approve',
+            # 'revisor3': None,
+            'revisor2': 'disapprove',
+        }
+        evaluators = []
+        for gmember in api.user.get_users(groupname='evaluators'):
+            # evaluators[gmember.id] = gmember.getProperty('fullname')
+            evaluators.append((gmember.getProperty('fullname'), values.get(gmember.id, None)))
+
+        return evaluators
