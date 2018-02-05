0c4d09a7bcd8dc1984540f679b143517be9488b3
gil-cano <gil.cano@gmail.com>
2018-02-05 11:11:54 -0600

    Register CV content type

---
+++ src/collective/judgment/content/files.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from plone.app.contenttypes.content import File
+from plone.dexterity.content import Container
+
+
+class CurriculumVitae(File):
+    """Convenience subclass for ``Curriculum Vitae`` portal type
+    """
