f38eb3f904b40ebc52defdee04e0e5409f2c8f09
adriana-rv <adriana.rv@gmail.com>
2018-02-05 21:01:00 -0600

    Add vocabularies

--- src/collective/judgment/vocabularies.py
+++ src/collective/judgment/vocabularies.py
@@ -15,3 +15,33 @@ def YesorNoVocabulary(context):
     items = [SimpleTerm(i[1], i[1], i[0]) for i in items]
     return SimpleVocabulary(items)
 directlyProvides(YesorNoVocabulary, IVocabularyFactory)
+
+
+def ClassificationsVocabulary(context):
+    items = [
+        (_(u'Researcher'), 'researcher'),
+        (_(u'Academic Technician'), 'academictec'),
+    ]
+
+    items = [SimpleTerm(i[1], i[1], i[0]) for i in items]
+    return SimpleVocabulary(items)
+directlyProvides(ClassificationsVocabulary, IVocabularyFactory)
+
+
+def PositionsVocabulary(context):
+    """Vocabulary for position."""
+    items = [
+        ('IAC', u'Investigador Asociado C'),
+        ('ITA', u'Investigador Titular A'),
+        ('ITB', u'Investigador Titular B'),
+        ('ITC', u'Investigador Titular C'),
+        ('TAAA', u'Técnico Académico Asociado A'),
+        ('TAAB', u'Técnico Académico Asociado B'),
+        ('TAAC', u'Técnico Académico Asociado C'),
+        ('TATA', u'Técnico Académico Titular A'),
+        ('TATB', u'Técnico Académico Titular B'),
+        ('TATC', u'Técnico Académico Titular C'),
+    ]
+    items = [SimpleTerm(i[0], i[0], i[1]) for i in items]
+    return SimpleVocabulary(items)
+directlyProvides(PositionsVocabulary, IVocabularyFactory)
