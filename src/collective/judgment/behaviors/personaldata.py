a1be2b464ba6a0d377f014a5fb9e1d18d024175f
adriana-rv <adriana.rv@gmail.com>
2018-02-06 18:45:27 -0600

    Delete description

--- src/collective/judgment/behaviors/personaldata.py
+++ src/collective/judgment/behaviors/personaldata.py
@@ -17,7 +17,6 @@ class IPersonalData(model.Schema):
     )
     title = schema.TextLine(
         title=u"Title",
-        description=u"A title, which will be converted to a name",
         required=True
     )
 
